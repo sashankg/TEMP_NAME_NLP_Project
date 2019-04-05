db884387f2b24031bb84b949a077d1e679acfa69
Jennifer <jjhuang1@andrew.cmu.edu>
2019-04-05 15:28:12 -0400

    removed unecessary files

--- location.py
+++
@@ -1,8 +0,0 @@
-import nltk
-import spacy
-import sys
-from BinQ import getVP, parser, getBinQ, leftmost, sentences
-
-parser.tagtype = 'ner'
-l = list(parser.tag(('Rami Eid is studying at Stony Brook University in NY'.split())))
-print(l)
