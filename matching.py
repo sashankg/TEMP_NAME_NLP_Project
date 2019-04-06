be6356f69fad9d5a0ecfc6fe45a80d7eb76c6e1d
Sashank Gogula <sashankg@gmail.com>
2019-04-06 00:05:45 -0400

    Added fuzzy search

---
+++ matching.py
@@ -0,0 +1,29 @@
+from nltk.stem.porter import *
+import nltk
+from fuzzywuzzy import fuzz
+import sys
+
+
+doc_filename = sys.argv[1]
+question = sys.argv[2]
+
+doc_file = open(doc_filename, 'r')
+
+doc = doc_file.read()
+
+sentences = nltk.sent_tokenize(doc)
+
+max_ratio = 0
+closest_sentence = ""
+
+stemmer = PorterStemmer()
+
+for s in sentences:
+    r = fuzz.ratio(s, question)
+    if r > max_ratio:
+        max_ratio = r
+        closest_sentence = s
+
+print(question)
+
+print(closest_sentence)
