889dcfc0c46503bbfb5cdead0db6343514163206
Kevin Wang <kevinw326@gmail.com>
2019-04-24 22:49:05 -0400

    Update What.py

--- What.py
+++ What.py
@@ -8,7 +8,7 @@ import sys
 nlp = spacy.load("en")
 lemmatizer = Lemmatizer(LEMMA_INDEX, LEMMA_EXC, LEMMA_RULES)
 
-
+"""
 def readFileLines(path):
     with open(path, 'r') as f:
         return f.readlines()
@@ -26,7 +26,7 @@ def getSentences(path):
             continue
         sentences += [str(sent) for sent in nl(txtline).sents]
     return sentences
-
+"""
 
 def is_tense(tag):
     return tag in ['VBD', 'VBZ', 'VBP', 'VBN']
@@ -188,7 +188,7 @@ def what(sent):
 
     return (final_question)
 
-
+"""
 def main():
     sents = getSentences("./training_data/a1.txt")
     for sent in sents:
@@ -198,3 +198,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+"""
